56222ed00371f32ccbd71e31322c6d57b73a5bd7
Vinicius <andrade.vini2019@gmail.com>
2024-11-09 21:27:45 -0300

    iniciando projeto

---
+++ organo-project/src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner (){
+    return(
+        // No JSX é className ao invés de class
+        <header className="banner">
+            <img src="/images/banner.jpg" alt="Banner principal"></img>
+        </header>
+        // public - imagens estáticas
+    )
+}export default Banner;
