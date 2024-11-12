806a30f95f3ebfdd8378780d21fb81235a1fc808
Vinicius Andrade <andrade.vini2019@gmail.com>
2024-11-12 07:15:50 -0300

    Criação do Dropdown

--- organo-project/src/components/Form/index.jsx
+++ organo-project/src/components/Form/index.jsx
@@ -1,7 +1,15 @@
 import './Form.css'
 import TextField from '../TextField/TextField'
+import Dropdown from '../Dropdown'
 
 const Form = () => {
+
+    const teams = [
+        'Poupatempo',
+        'Detran',
+        'Sabesp'
+    ]
+
     return(
         <section className='form'>
             <form>
@@ -9,6 +17,7 @@ const Form = () => {
                 <TextField label="Nome" placeholder="Digite seu nome"/>
                 <TextField label="Cargo" placeholder="Digite seu cargo"/>
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <Dropdown label="Times" itens={teams}/>
                 <button>Enviar</button>
             </form>
         </section>
